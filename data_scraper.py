189881684b4aa2ba87dcaeb668e6fc5738dd080f
Maggie Angst <maggieangst@Maggies-MacBook-Pro-6.local>
2016-04-10 22:33:19 -0500

    add data_scraper.py

--- data_scraper.py
+++ data_scraper.py
@@ -13,6 +13,9 @@ br.open('http://enr.sos.mo.gov/EnrNet/CountyResults.aspx')
 # Put this up here so you can loop over it
 option_values = ['001', '003', '005', '007', '009', '011', '013', '015', '017', '019', '021', '023', '025', '027', '029', '031', '033', '035', '037', '039', '041', '043', '045', '047', '049', '051', '053', '055', '057', '059', '061', '063', '065', '067', '069', '071', '073', '075', '077', '079', '081', '083', '085', '087', '089', '091', '093', '095', '097', '099', '101', '095A', '103', '105', '107', '109', '111', '113', '115', '117', '119', '121', '123', '125', '127', '129', '131', '133', '135', '137', '139', '141', '143', '145', '147', '149', '151', '153', '155', '157', '159', '161', '163', '165', '167', '169', '171', '173', '175', '177', '179', '181', '195', '197', '199', '201', '203', '205', '183','185', '187', '189', '510', '188', '207','209', '211', '213', '215', '217', '219', '221', '223', '225', '227', '229']
 i = 0
+
+county_names = ['Adair', 'Andrew', 'Atchison', 'Audrain', 'Barry', 'Barton', 'Bates', 'Benton', 'Bollinger', 'Boone', 'Buchanan', 'Butler', 'Caldwell', 'Callaway', 'Camden', 'Cape Girardeau', 'Carroll', 'Carter', 'Cass', 'Cedar', 'Chariton', 'Christian', 'Clark', 'Clay', 'Clinton', 'Cole', 'Cooper', 'Crawford', 'Dade', 'Dallas', 'Daviess', 'De Kalb', 'Dent', 'Douglas', 'Dunklin', 'Franklin', 'Gasconade', 'Gentry', 'Greene', 'Grundy', 'Harrison', 'Henry', 'Hickory', 'Holt', 'Howard', 'Howell', 'Iron', 'Jackson', 'Jasper', 'Jefferson', 'Johnson', 'Kansas City', 'Knox', 'Laclede', 'Lafayette', 'Lawrence', 'Lewis', 'Lincoln', 'Linn', 'Livingston', 'Macon', 'Madison', 'Maries', 'Marion', 'McDonald', 'Mercer', 'Miller', 'Mississippi', 'Moniteau', 'Monroe', 'Montgomery', 'Morgan', 'New Madrid', 'Newton', 'Nodaway', 'Oregon', 'Osage', 'Ozark', 'Pemiscot', 'Perry', 'Pettis', 'Phelps', 'Pike', 'Platte', 'Polk', 'Pulaski', 'Putnam', 'Ralls', 'Randolph', 'Ray', 'Reynolds', 'Ripley', 'Saline', 'Schuyler', 'Scotland', 'Scott', 'Shannon', 'Shelby', 'St. Charles', 'St. Clair', 'St. Francois', 'St. Louis', 'St. Louis City', 'Ste. Genevieve', 'Stoddard', 'Stone', 'Sullivan', 'Taney', 'Texas', 'Vernon', 'Warren', 'Washington', 'Wayne', 'Webster', 'Worth', 'Wright']
+i = 0
 # You want to loop over that option_values list and basically execute almost the exact code we already went over
 # in class. Difference is, you're executing it multiple times -- one for each county.
 
@@ -22,7 +25,8 @@ for value in option_values:
     br.form['ctl00$MainContent$cboElectionNames'] = ['750003566'] # Same as example
 
     # Now do the same here with the county dropdown and set it to the value of the loop iteration.
-    br.form['ctl00$MainContent$cboCounty'] = [option_values[i]]
+    br.form['ctl00$MainContent$cboCounty'] = [option_values[i]] 
+    [option_values[i]] = [county_names[i]]
 
     # You got this part right. That's the button you want to submit. Again, similar to class example.
     br.submit('ctl00$MainContent$btnCountyChange')
@@ -41,12 +45,13 @@ for value in option_values:
     )
 
     # Now get the data from each table row
+
     for row in main_table.find_all('tr'):
         data = [cell.text for cell in row.find_all('td')]
         
         if data:
             if data[0] in ['Hillary Clinton', 'Ted Cruz', 'Donald J. Trump', 'Bernie Sanders', 'John R. Kasich']: 
-                print option_values[i], data[0], data[3]
+                print county_names[i], data[0], data[3]
          #writer.writerow(data)
     
     i=i+1
